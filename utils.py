9a630163602ab1844511a9df267c5542cc0d607f
mark-marshall <mark.marshallgp@gmail.com>
2019-08-29 17:28:45 +0100

    Designed shape of pricingRules and deliveryRules data

---
+++ utils.py
@@ -0,0 +1,12 @@
+pricingRules = {
+        'A': {'price': 8, 'discountFrequency': 0, 'discountedPrice': 0},
+        'B': {'price': 12, 'discountFrequency': 2, 'discountedPrice': 20},
+        'C': {'price': 4, 'discountFrequency': 3, 'discountedPrice': 10},
+        'D': {'price': 7, 'discountFrequency': 2,'discountedPrice': 7},
+        'E': {'price': 5, 'discountFrequency': 3,'discountedPrice': 10},
+        }
+
+deliveryRules = {
+        'standard': 7,
+        'freeThreshold': 50
+        }
